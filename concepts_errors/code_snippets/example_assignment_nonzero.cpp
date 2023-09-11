b12da4730ac25acfb134ab76786206f013693277
Diego-Benito <61522831+Diego-Benito@users.noreply.github.com>
2023-09-11 21:55:51 +0200

    asdf

---
+++ concepts_errors/code_snippets/example_assignment_nonzero.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i;
+    cout << (i = 22) << endl; 
+
+    string dna = "AAT";
+    cout << ( dna[1] = 'r' ) << endl;
+
+}
---
+++ concepts_errors/memory_allocation/t.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int one() {
+    string a = "hello";
+    cout << a << '\n';
+}
+
+int two() {
+    string a;
+    a= "hello";
+    cout << a << '\n';
+}
